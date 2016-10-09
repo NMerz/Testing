18fbffed719a6c281fc1d856cc2aec6b9f1bff21
Derionicle <nmerz@icloud.com>
2016-10-09 19:48:03 -0400

    more testing

--- TableViewController.swift
+++ TableViewController.swift
@@ -27,24 +27,21 @@ class TableViewController: UITableViewController {
         return myarray.count
     }
     
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        /*let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath) //as! UITableViewCell
+    /*override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath) as UITableViewCell
          cell.textLabel?.text = myarray[indexPath.item]
          return cell
-         let cell = UITableViewCell()
+         /*let cell = UITableViewCell()
          let label = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
          label.text = "test"
          cell.addSubview(label)
          return cell*/
         //let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath)
-        let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath) as UITableViewCell
+        /*let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath) as UITableViewCell
         let label = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
         label.text = "test"
         cell.addSubview(label)
-        return cell
-    }
+        return cell*/
+    }*/
+    
 }
