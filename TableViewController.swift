1ddbe8cba89980b1be5a16949590dadc49ab255d
Derionicle <nmerz@icloud.com>
2016-10-10 20:45:56 -0400

    Table working
    The tableView loading is now working correctly. The next step will be
    to make each cell link to a new screen

--- TableViewController.swift
+++ TableViewController.swift
@@ -12,10 +12,14 @@ import UIKit
 class TableViewController: UITableViewController {
     //@IBOutlet var tableView: UITableView!
     let myarray = ["item1", "item2", "item3"]
+    let images = ["homesteadhigh"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "customcell")
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,13 +28,17 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(tableView:UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myarray.count
+        return self.myarray.count;
     }
     
-    /*override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath) as UITableViewCell
-         cell.textLabel?.text = myarray[indexPath.item]
-         return cell
+        cell.textLabel?.text = myarray[indexPath.item]
+        if indexPath.item<images.count {
+            let image : UIImage = UIImage(named: images[indexPath.item])!
+            cell.imageView?.image = image
+        }
+        return cell
          /*let cell = UITableViewCell()
          let label = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
          label.text = "test"
@@ -42,6 +50,6 @@ class TableViewController: UITableViewController {
         label.text = "test"
         cell.addSubview(label)
         return cell*/
-    }*/
+    }
     
 }
